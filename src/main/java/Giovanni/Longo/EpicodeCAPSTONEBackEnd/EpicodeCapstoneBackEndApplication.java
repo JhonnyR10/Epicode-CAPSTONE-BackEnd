68f43336bd47f9ac4e474fb58beec8938857895c
Giovanni Longo <giovanni.longo10@icloud.com>
2024-02-07 16:16:58 +0100

    create project

---
+++ src/main/java/Giovanni/Longo/EpicodeCAPSTONEBackEnd/EpicodeCapstoneBackEndApplication.java
@@ -0,0 +1,13 @@
+package Giovanni.Longo.EpicodeCAPSTONEBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicodeCapstoneBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicodeCapstoneBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Giovanni/Longo/EpicodeCAPSTONEBackEnd/EpicodeCapstoneBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package Giovanni.Longo.EpicodeCAPSTONEBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicodeCapstoneBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
